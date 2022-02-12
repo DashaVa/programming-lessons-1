ec3875255560db48bf4960219ef7513ab21da45e
DashaVa <dashka.va@yandex.ru>
2022-02-12 11:42:17 +0300

    Добавила ФИО в README

---
+++ typeof.js
@@ -0,0 +1,14 @@
+let name = "Vakhrusheva";
+console.log(typeof name,name);
+let age = 21;
+console.log(typeof age,age);
+let isGreater = 4 > 1;
+console.log( typeof isGreater,isGreater);
+let a = [123];
+console.log(typeof a,a);
+let d=Symbol('//')
+console.log(typeof d,d)
+let g;
+console.log(typeof g,g);
+
+
