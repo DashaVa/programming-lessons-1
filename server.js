83cd21ed2e319bebc6e7a016b1ae95ffdcbd205f
DashaVa <dashka.va@yandex.ru>
2022-03-03 12:42:43 +0300

    файл hello

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const fs = require("fs");
 const http = require("http");
 const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     if (request.url === "/home" || request.url === "/") {
         response.write("<h2>Class work</h2>");
+        fs.appendFileSync("hello.txt","переход в home\n");
     } else if (request.url == "/about") {
         response.write("<h2>About</h2>");
+        fs.appendFileSync("hello.txt","переход в about\n");
     } else if (request.url == "/contact") {
         response.write("<h2>Contacts</h2>");
+        fs.appendFileSync("hello.txt","переход в contacts\n")
     } else {
         response.write("<h2>Not found</h2>");
     }
