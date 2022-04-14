aef0cc7ab1e163af9db4b87776503fe874ec5829
DashaVa <dashka.va@yandex.ru>
2022-04-14 16:55:24 +0300

    отправляем по запросу на сервер один элемент файла

--- pract2.js
+++ pract2.js
@@ -12,11 +12,37 @@ fs.writeFile('code.json',json,(err)=>{
         if (err) return reject(err);
         console.log('wrote');
         let arr = JSON.parse(json)
-        for (k = 0; k < arr.length; ++k){
-            console.log(arr[k])
-        }
+        console.log(typeof(arr));
+        let mas = Object.fromEntries(
+           Object.entries(arr).map(([key,value])=>[key,value])
+        );
+        console.log(typeof(mas));
+        console.log(mas[0]);
+        var string = JSON.stringify(mas[0])
+const requestHandler = (request, response) => {
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    if (request.url === "/todos/1" || request.url === "/") {
+        response.write(string);
+        fs.appendFileSync("hello.txt","переход в home\n");
+    } else if (request.url == "/about") {
+        response.write("<h2>About</h2>");
+        fs.appendFileSync("hello.txt","переход в about\n");
+    }else {
+        response.write("<h2>Not found</h2>");
+    }
+    response.end();
+};
+http.createServer(requestHandler).listen(3000); 
+        //var counter = 0;
+        //for (let key of Object.values(arr)){
+//console.log(key);
+        //}
+        //console.log(key + arr[key]);
+        //for (k = 0; k < arr.length; ++k){
+            //console.log(k + arr[k])
+        //}
 
         //console.log(json.search('delectus'))
     })
 })
-.catch(e => console.error(e));
+.catch(e => console.error(e))
